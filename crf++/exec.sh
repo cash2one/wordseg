646aee8007d53b5b41d44c2144c2929bb3819995
gdanskamir <759575836@qq.com>
2016-04-24 02:55:01 +0800

    crf train template

--- crf++/exec.sh
+++ crf++/exec.sh
@@ -1,5 +1,4 @@
 #!/usr/bin
 
-time ./bin/crf_learn -f 3 -c 4.0 template 4.train.data 4.model > 4.train.rst  
-time ./bin/crf_test -m 4.model 4.test.data > 4.test.rst  
-  
+time ./bin/crf_learn -f 3 -c 4.0 -p 12 template 4.train.data 4.model > 4.train.rst
+time ./bin/crf_test -m 4.model 4.test.data > 4.test.rst
